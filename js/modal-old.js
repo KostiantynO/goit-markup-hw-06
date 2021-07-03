660c1ee375c30df70bbfa4cc7a5bce8a782ecae1
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-07-04 02:18:17 +0300

    Adds minified version modal.min.js and adds a new class ".no-scroll" to html
    Adds minified version of original script for modal window.
    Now adds a new class ".no-scroll" to .page-html for smooth scrolling experience.

---
+++ js/modal-old.js
@@ -0,0 +1,17 @@
+// @ts-check
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    noScroll: document.querySelector("[data-modal-no-scroll]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.noScroll.classList.toggle("no-scroll");
+  }
+})();
--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,12 @@
-// @ts-check
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+  const e = {
+    z: document.querySelector("[data-modal-open]"),
+    x: document.querySelector("[data-modal-close]"),
+    c: document.querySelector("[data-modal]"),
+    t: document.querySelector("[data-no-scroll]"),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function p() {
+    e.c.classList.toggle("is-hidden"), e.t.classList.toggle("no-scroll");
   }
+  e.z.addEventListener("click", p), e.x.addEventListener("click", p);
 })();
