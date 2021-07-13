b7649fa469876bee08988806e60d64cb5ffc1a3f
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-07-13 06:58:21 +0300

    adds 3d

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,25 @@
-(() => {
+(function () {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    modalTitle: document.querySelector(".modal-title"),
+    modalTitle: document.querySelector("[data-modal-title]"),
     htmlAndBody: document.querySelectorAll("[data-no-scroll]"),
   };
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    refs.htmlAndBody[0].classList.toggle("no-scroll");
-    refs.htmlAndBody[1].classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is-hidden"),
+      refs.htmlAndBody[0].classList.toggle("no-scroll"),
+      refs.htmlAndBody[1].classList.toggle("no-scroll");
   }
 
+  var card = document.querySelector(".card");
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
+  });
+
   refs.openModalBtn.addEventListener("click", function () {
-    toggleModal(), modalTitle.focus();
+    toggleModal(), refs.modalTitle.focus();
   }),
     refs.closeModalBtn.addEventListener("click", toggleModal),
     refs.modal.addEventListener("click", function (event) {
